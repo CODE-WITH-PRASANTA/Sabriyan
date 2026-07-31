567892067f6d248a65e422a95f2e0d920d104168
Pritam barik <barickpritam08@gmail.com>
2026-07-31 16:04:03 +0530

    done

--- AdminPannel/src/App.jsx
+++ AdminPannel/src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Global Green & Golden Theme Styles
+import "./App.css";
+
 // Layout
 import MainLayout from "./Layout/MainLayout/MainLayout";
 import AddnewProduct from "./Pages/AddnewProduct/AddnewProduct";
@@ -15,37 +18,39 @@ import Attributes from "./Components/Attributes/Attributes";
 import CompletedOrders from "./Components/CompletedOrders/CompletedOrders";
 import CancelOrders from "./Components/CancelOrders/CancelOrders";
 import Customers from "./Components/Customers/Customers";
+import BlogPost from "./Components/BlogPost/BlogPost";
+import BlogManagement from "./Components/BlogManagement/BlogManagement";
+import ColdLeads from "./Components/ColdLeads/ColdLeads";
 
 function App() {
   return (
-    <Router>
-      <Routes>
+    <div className="App-theme-wrapper">
+      <Router>
+        <Routes>
+          {/* All admin pages use MainLayout */}
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="/products/add" element={<AddnewProduct />} />
+            <Route path="/products/all" element={<AllProduct />} />
+            <Route path="/orders/all" element={<AllOrder />} />
 
-        {/* All admin pages use MainLayout */}
-        <Route path="/" element={<MainLayout />}>
+            <Route path="products/categories" element={<Catagories />} />
+            <Route path="products/brands" element={<ManageBrands />} />
+            <Route path="products/attributes" element={<Attributes />} />
 
-          <Route index element={<Dashboard />} />
-          <Route path="dashboard" element={<Dashboard />} />
-<<<<<<< HEAD
-          <Route path="/products/add" element={<AddnewProduct/>}/>
-          <Route path="/products/all"element={<AllProduct/>}/>
-          <Route path="/orders/all"element={<AllOrder/>}/>
-=======
+            <Route path="orders/complete" element={<CompletedOrders />} />
+            <Route path="orders/cancel" element={<CancelOrders />} />
 
-          <Route path="products/categories" element={<Catagories />} />
-          <Route path="products/brands" element={<ManageBrands />} />
-          <Route path="products/attributes" element={<Attributes />} />
+            <Route path="customers" element={<Customers />} />
+            <Route path="/blogpost" element={<BlogPost />} />
+            <Route path="/blogmanagement" element={<BlogManagement />} />
+            <Route path="/coldleads" element={<ColdLeads />} />
 
-          <Route path="orders/complete" element={<CompletedOrders />} />
-          <Route path="orders/cancel" element={<CancelOrders />} />
-
-          <Route path="customers" element={<Customers />} />
-
->>>>>>> 1e9cf53c16988aedd2d2781e35190c7aa7ae3cec
-        </Route>
-
-      </Routes>
-    </Router>
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
